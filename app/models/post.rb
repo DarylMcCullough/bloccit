9baf6a6f317222093386096d0837b7f75b3c80d7
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-03-13 18:43:25 +0000

    testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
     has_many :comments
+    
+    def mark_spam
+        if self.id % 5 == 1
+            self.title = "SPAM"
+        end
+    end
+    
+    def test
+        puts "Just testing"
+    end
 end
